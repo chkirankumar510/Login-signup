7fa0da6911b9a5555d21a5ac264067e5121ce41b
chkirankumar510 <30830071+chkirankumar510@users.noreply.github.com>
2020-07-28 07:28:06 +0530

    Login

---
+++ Login.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>Login</title>
+  </head>
+  <h3>add entry</h3>
+  <p>add another article</p>
+  <form action="trylog.php" method="post">
+    <label for="username">Username</label><input type="username" id="username" name="username"><br /><br />
+    <label for="password">Password:</label><input type="password" id="password" name="password"><br /><br />
+    <button type="submit">Login</button>
+  </form>
+</html>
